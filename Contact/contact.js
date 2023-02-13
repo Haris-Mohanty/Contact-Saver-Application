60bf22f85afcb6a4d77376679e8f336fd8ebe83e
Haris Mohanty <108195360+Haris-Mohanty@users.noreply.github.com>
2023-02-13 06:29:26 +0000

    LogOut code fixed.

--- Contact/contact.js
+++ Contact/contact.js
@@ -1,10 +1,15 @@
 //Get Session storage data
+let username = sessionStorage.getItem("username");
+if(username == null){
+  document.body.innerHTML = "<h1>404 Not Found!</h1>";
+  document.body.classList.add("illigal");
+}
 // LOGOUT CODE START
 let logoutBtn = document.getElementById("logout-btn");
 logoutBtn.style.cursor = "pointer";
 logoutBtn.style.color = "red";
 logoutBtn.addEventListener("click", () => {
 window.location ="../index.html";
-  
+sessionStorage.removeItem("username"); 
 })
 // LOGOUT CODE END
