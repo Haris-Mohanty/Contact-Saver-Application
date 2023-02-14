7c19b00f8bd60879c45ad82028dc1334473159a1
Haris Mohanty <108195360+Haris-Mohanty@users.noreply.github.com>
2023-02-14 03:58:14 +0000

    add new contact and show code processing....

--- Contact/contact.js
+++ Contact/contact.js
@@ -1,6 +1,6 @@
 //Get Session storage data
 let username = sessionStorage.getItem("username");
-if(username == null){
+if (username == null) {
   document.body.innerHTML = "<h1>404 Not Found!</h1>";
   document.body.classList.add("illigal");
 }
@@ -9,17 +9,29 @@ let logoutBtn = document.getElementById("logout-btn");
 logoutBtn.style.cursor = "pointer";
 logoutBtn.style.color = "red";
 logoutBtn.addEventListener("click", () => {
-window.location ="../index.html";
-sessionStorage.removeItem("username"); 
+  window.location = "../index.html";
+  sessionStorage.removeItem("username");
 })
 // LOGOUT CODE END
 
 //Welcome code Start
 let welcome = document.getElementById("welcome");
 let user_data = JSON.parse(localStorage.getItem(username));
-welcome.innerHTML ="Welcome "+user_data.f_name +" "+ user_data.l_name;
+welcome.innerHTML = "Welcome " + user_data.f_name + " " + user_data.l_name;
 //Welcome code End
 
 //CREATE CONTACT CODE START
 let createBtn = document.querySelector(".create-btn");
+let contact_detail = document.querySelector(".contact-details");
+
+createBtn.onclick = function(e) {
+  e.preventDefault();
+  newContactAdd();
+}
+
+const newContactAdd = () => {
+let accordion = document.createElement("DIV");
+  accordion.classList = "accordion mb-3";
+  
+}
 //CREATE CONTACT CODE END
