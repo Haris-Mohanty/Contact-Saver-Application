5f490bb66cb1de17d6a5ade8ac7c54e40ee9941c
Haris Mohanty <108195360+Haris-Mohanty@users.noreply.github.com>
2023-03-02 02:56:41 +0000

    Option button (3 dot) code :- After click the 3 dot edit and delete option showing successful.

--- Contact/contact.js
+++ Contact/contact.js
@@ -57,61 +57,52 @@ function newContactAdd(task) {
     number = task.co_number;
   }
 //SHOWING CONTACT IN PAGE FROM LOCALSTORAGE CODE END
-  //Accordion Create
-let accordion = document.createElement("DIV");
-  accordion.classList = "accordion mb-3";
-  
 //for new id 
 let all_accordion = contact_detail.getElementsByClassName("accordion");
   for(i=0; i<all_accordion.length; i++){
     
   }
   
-let accordion_item = document.createElement("DIV");
-  accordion_item.classList = "accordion-item";
-  accordion.append(accordion_item);
-let accordion_header = document.createElement("H5");
-accordion_header.classList = "accordion-header";
-  accordion_item.append(accordion_header);
-let accordion_button = document.createElement("BUTTON");
-accordion_button.classList = "accordion-button";
-  accordion_button.innerText = name;
-  accordion_button.setAttribute("data-bs-toggle", "collapse");
-  accordion_button.setAttribute("data-bs-target", "#collapse-"+i);
-  accordion_header.append(accordion_button);
-let accordion_collapse = document.createElement("DIV");
- accordion_collapse.classList = "accordion-collapse collapse";
-  accordion_collapse.id = "collapse-"+i;
-  accordion_item.append(accordion_collapse);
-let accordion_body = document.createElement("DIV");
- accordion_body.classList = "accordion-body";
-  accordion_collapse.append(accordion_body);
-let row = document.createElement("DIV");
- row.classList = "row";
-  accordion_body.append(row);
-let col1 = document.createElement("DIV");
- col1.classList = "col-md-6";
-  row.append(col1);
-let h5 = document.createElement("H5");
-  h5.innerText = name;
-  col1.append(h5);
-let p = document.createElement("P");
-  p.innerText = number;
-  col1.append(p);
-let col2 = document.createElement("DIV");
- col2.classList = "col-md-6 d-flex justify-content-around align-items-center position-relative";
-col2.innerHTML = '<i class="fa-regular fa-message"></i><i class="fa-solid fa-square-phone"></i><i class="fa-solid fa-ellipsis-vertical op-btn"></i>';
-  row.append(col2);
-let option_box = document.createElement("DIV");
- option_box.classList = "option-box";
-option_box.innerHTML = '<i class="fa-regul]ar fa-pen-to-square"></i><i class="fa-solid fa-trash"></i>';
-col2.append(option_box);
+  //ACCORDION CODE START
+
+contact_detail.innerHTML += `
+ <div class="accordion mb-3">
+       <div class="accordion-item">
+         <h4 class="accordion-header">
+           <button class="accordion-button" data-bs-toggle="collapse" 
+           data-bs-target="#collapse-${i}">${name}
+            </button>
+         </h4>
+         <div class="accordion-collapse collapse" id="collapse-${i}">
+            <div class="accordion-body">
+               <div class="row">
+                <div class="col-md-6">
+                   <h5>${name}</h5>
+                   <p>${number}</p>
+                 </div>
+                 <div class="col-md-6 d-flex justify-content-around
+                       align-items-center position-relative">
+                       <i class="fa-regular fa-message"></i>
+                       <i class="fa-solid fa-square-phone"></i>
+                       <i class="fa-solid fa-ellipsis-vertical op-btn"></i>
+                       
+                        <div class="option-box">
+                         <i class="fa-regular fa-pen-to-square mb-1"></i>
+                         <i class="fa-solid fa-trash"></i>
+                        </div>
+                 </div>
+                </div>
+             </div>
+           </div>
+       </div>
+ </div>
+`;
   
-  contact_detail.append(accordion);
+  //ACCORDION CODE END
   
   input_name.value = "";
   input_number.value = "";
-
+  
   //OPTION BUTTON(3 cDOT) CODE START
   let op_btn = document.querySelectorAll(".op-btn");
   for(i=0; i<op_btn.length; i++){
@@ -119,6 +110,7 @@ col2.append(option_box);
       let parent = this.parentElement;
       let op_box = parent.querySelector(".option-box");
      op_box.classList.add("active");
+     op_box.classList.add("anim");
     }
   }
   //OPTION BUTTON(3 DOT) CODE END
@@ -135,10 +127,11 @@ function updateLocalStorage(){
     let h5 = accordion_el[i].getElementsByTagName("H5");
     let p = accordion_el[i].getElementsByTagName("P");
     array_list.push({
-      co_name : h5[1].innerHTML,
+      co_name : h5[0].innerHTML,
       co_number : p[0].innerHTML
     });
   }
+  
   localStorage.setItem(username+"_list", JSON.stringify(array_list));
 }
 
